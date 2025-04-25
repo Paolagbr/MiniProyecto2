556e48b540b5ac59f31be12ce6aec02407466aff
Paolagbr <150736512+Paolagbr@users.noreply.github.com>
2025-04-25 15:01:06 -0600

    Tienda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { CarruselComponent } from "./components/carrusel/carrusel.component";
@@ -7,7 +7,7 @@ import { CarruselComponent } from "./components/carrusel/carrusel.component";
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, CarruselComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, CarruselComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TiendaComponent } from './tienda/tienda.component';
+import { UnProductoComponent } from './un-producto/un-producto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'Tienda', component: TiendaComponent},
+    {path:'Tienda/:id', component: UnProductoComponent},
+];
