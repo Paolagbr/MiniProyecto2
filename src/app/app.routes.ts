239a6e659279685411f3e778de331efc7541d980
Paolagbr <150736512+Paolagbr@users.noreply.github.com>
2025-04-25 15:36:16 -0600

    RedireccionInicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { TiendaComponent } from './tienda/tienda.component';
 import { UnProductoComponent } from './un-producto/un-producto.component';
+import { CarruselComponent } from './components/carrusel/carrusel.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+    { path: 'inicio', component: CarruselComponent },
     {path:'Tienda', component: TiendaComponent},
     {path:'Tienda/:id', component: UnProductoComponent},
 ];
