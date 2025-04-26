281f337be8ffc674deb9e208a108c835539ef1af
Alondra <alondracha04@gmail.com>
2025-04-26 16:06:15 -0600

    Nosotros

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { TiendaComponent } from './tienda/tienda.component';
 import { UnProductoComponent } from './un-producto/un-producto.component';
 import { CarruselComponent } from './components/carrusel/carrusel.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/inicio', pathMatch: 'full' },
     { path: 'inicio', component: CarruselComponent },
     {path:'Tienda', component: TiendaComponent},
     {path:'Tienda/:id', component: UnProductoComponent},
+    { path: 'Nosotros', component: NosotrosComponent }
 ];
