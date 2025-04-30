484ec41f4799942c57779118fa8ed47bd105ea6a
Alondra <alondracha04@gmail.com>
2025-04-29 18:07:50 -0600

    FormsReactivo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { TiendaComponent } from './tienda/tienda.component';
 import { UnProductoComponent } from './un-producto/un-producto.component';
 import { CarruselComponent } from './components/carrusel/carrusel.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
+import { FormularioReactivoComponent } from './components/formulario-reactivo/formulario-reactivo.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/inicio', pathMatch: 'full' },
     { path: 'inicio', component: CarruselComponent },
     {path:'Tienda', component: TiendaComponent},
     {path:'Tienda/:id', component: UnProductoComponent},
-    { path: 'Nosotros', component: NosotrosComponent }
+    {path: 'Nosotros', component: NosotrosComponent },
+    {path: 'formulario-reactivo', component: FormularioReactivoComponent}
 ];
